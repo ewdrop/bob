fa4e67409afb7d78af04741d259c4ba45651fb11
ewdrop <droprainy@outlook.com>
2026-07-03 09:27:06 +0800

    链表专题：判断环形链表是否有环

--- src/main/java/com/wwj/lianxi/App.java
+++ src/main/java/com/wwj/lianxi/App.java
@@ -10,8 +10,19 @@ import java.util.*;
 
 public class App {
 
+    public static class ListNode{
+              int val;
+              ListNode next;
+              ListNode() {}
+              ListNode(int val) {
+                  this.val = val;
+              }
+              ListNode(int val, ListNode next) {
+                  this.val = val;
+                  this.next = next;
+              }
+          }
     public static void main(String[] args) {
-
 
     }
 
---
+++ src/main/java/com/wwj/lianxi/algorithm/DetectCycle.java
@@ -0,0 +1,36 @@
+package com.wwj.lianxi.algorithm;
+
+public class DetectCycle {
+    /**
+     * ListNode head = new ListNode(1);
+     *         head.next = new ListNode(2);
+     *         head.next.next = new ListNode(3);
+     *         head.next.next.next = new ListNode(4);
+     *         head.next.next.next.next = new ListNode(5);
+     *         head.next.next.next.next.next = head.next.next;
+     *         ListNode result = ringList(head);
+     *         try {
+     *             System.out.println(result.val);
+     *         } catch (NullPointerException e) {
+     *             System.out.println(result);
+     *         }
+     *      public static ListNode detectCycle(ListNode head) {
+     *         if (head == null || head.next == null) return null;
+     *         ListNode slow = head;
+     *         ListNode fast = head;
+     *         while (fast != null && fast.next != null) {
+     *              fast = fast.next.next;
+     *              slow = slow.next;
+     *             if (fast == slow) {
+     *                 slow = head;
+     *                 while (slow != fast) {
+     *                     slow = slow.next;
+     *                     fast = fast.next;
+     *                 }
+     *                 return slow;
+     *             }
+     *         }
+     *         return null;
+     *     }
+     */
+}
