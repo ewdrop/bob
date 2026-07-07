d4897fb723008f0c8c748be0994d7d70a826f8a8
ewdrop <droprainy@outlook.com>
2026-07-07 09:40:34 +0800

    二叉树前序遍历

---
+++ src/main/java/com/wwj/lianxi/algorithm/tree/InorderTraversal.java
@@ -0,0 +1,56 @@
+package com.wwj.lianxi.algorithm.tree;
+
+public class InorderTraversal {
+    /**
+     * public static class TreeNode{
+     *         int val;
+     *         TreeNode left;
+     *         TreeNode right;
+     *
+     *         public TreeNode() {
+     *         }
+     *
+     *         public TreeNode(int val) {
+     *             this.val = val;
+     *         }
+     *
+     *         public TreeNode(int val, TreeNode left, TreeNode right) {
+     *             this.val = val;
+     *             this.left = left;
+     *             this.right = right;
+     *         }
+     *     }
+     *     <----------------中序遍历，迭代实现------------------>
+     *     public static List<Integer> inorderTraversal(TreeNode root) {
+     *         List<Integer> result = new ArrayList<>();
+     *         Stack<TreeNode> stack = new Stack<>();
+     *         TreeNode current = root;
+     *         while (current != null || !stack.isEmpty()) {
+     *             while (current != null) {
+     *                 stack.push(current);
+     *                 current = current.left;
+     *             }
+     *             current = stack.pop();
+     *             result.add(current.val);
+     *
+     *             current = current.right;
+     *         }
+     *         return result;
+     *     }
+     *      <----------------中序遍历，递归实现------------------>
+     *     public static List<Integer> inorderTraversal2(TreeNode root) {
+     *         List<Integer> result = new ArrayList<>();
+     *         inorderHelper(root, result);
+     *         return result;
+     *     }
+     *
+     *     private static void inorderHelper(TreeNode root, List<Integer> result) {
+     *         if (root == null) {
+     *             return;
+     *         }
+     *         inorderHelper(root.left, result);
+     *         result.add(root.val);
+     *         inorderHelper(root.right, result);
+     *     }
+     */
+}
